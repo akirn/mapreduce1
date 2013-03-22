0960fccdf073abf34ebeb760838e12333ea7ec52
Andreas Kirn <akirn@switcheye.com>
2013-03-22 10:27:01 -0700

    map on a key

--- mapper.py
+++ mapper.py
@@ -7,6 +7,4 @@ counts = {}
 for line in sys.stdin:
     words = line.split()
     for word in words:
-        counts[word] = counts.get(word, 0) + 1
-
-print(counts)
+        print(word + "\t" + str(1))
