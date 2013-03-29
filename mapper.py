bcd2891be77533325f25d6e94ec8f942085f463a
Andreas Kirn <akirn@switcheye.com>
2013-03-28 22:11:26 -0700

    remove commas and other punctuation marks from words

--- mapper.py
+++ mapper.py
@@ -1,10 +1,14 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python
 
 import sys
+import re
 
 counts = {}
+regex = re.compile('[a-zA-Z]+')
 
 for line in sys.stdin:
     words = line.split()
     for word in words:
-        print(word + "\t" + str(1))
+        m = regex.match(word)
+        if m is not None:
+            print(m.group().lower() + "\t" + str(1))
